3af55b5c7e6f7f632408100b94b59c8f03708dba
cassiaavila <sdcassia2@gmail.com>
2021-06-13 17:52:58 -0300

    Criei uma classe principal e uma secundária

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double n1;
+        double n2;
+        String verificarOperacao;
+
+        Operacoes operacoes = new Operacoes();
+        Scanner teclado= new Scanner(System.in);
+
+        System.out.println("Digite um número");
+        n1 = teclado.nextDouble();
+
+
+        System.out.println("Digite o segundo número");
+        n2 = teclado.nextDouble();
+
+        System.out.println("Escolha a operação");
+        verificarOperacao = teclado.nextLine();
+
+        if( verificarOperacao == "+"){
+            operacoes.Somar(n1, n2);
+
+        } if (verificarOperacao == "-"){
+            operacoes.Subtrair(n1, n2);
+        } if (verificarOperacao == "*"){
+            operacoes.multiplicar(n1, n2);
+        } if (verificarOperacao == "/"){
+            operacoes.dividir(n1, n2);
+
+        }
+
     }
 }
